68016968448e385a117b3204307f7d4ebb72cc57
Alfatehan Arsya Baharin <alfatehanarsya8998@gmail.com>
2021-04-14 14:36:48 +0700

    Menambahkan Konsep OOP
    Menambahkan beberapa class:
    - Bmi
    - Name
    - Array

---
+++ BasicProgrammingOOP/BasicProgrammingOOP/Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgrammingOOP
+{
+    class Array
+    {
+        public int size;
+
+        public Array(int aSize)
+        {
+            size = aSize;
+            int[] intArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Masukkan angka index ke-{i}");
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum = intArray.Sum();
+            Console.WriteLine($"jumlah dari array adalah {sum}");
+        }
+    }
+}
